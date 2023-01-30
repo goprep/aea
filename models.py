1a047cec335814d1bc4b97317347615e9d900440
jstsss <jhasan@iastate.edu>
2023-01-29 19:57:51 -0600

    a
    f

---
+++ models.py
@@ -0,0 +1,18 @@
+
+import torch
+class CustomDataset:
+	def __init__(self, data, targets):
+		self.data = data
+		self.targets = targets
+
+	def __len__(self):
+		return len(self.data)
+
+	def __getitem__(self, idx):
+		current_sample = self.data[idx, :] # all rows [:]
+		current_target = self.targets[idx]
+
+		return {
+			"sample": torch.tensor(current_sample, dtype=torch.float),
+			"target": torch.tensor(current_target, dtype=torch.long),
+		}
